cda3bbeb5d293ef0581e2af55950415e0a040077
sayale7 <thomas.kohler@gmx.com>
2010-02-23 00:41:27 +0100

    j

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,12 +32,12 @@ set :branch, "master"
 
 after "deploy:symlink" #, "deploy:update_crontab"
 
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
-end
+# namespace :deploy do
+#   desc "Update the crontab file"
+#   task :update_crontab, :roles => :db do
+#     run "cd #{release_path} && whenever --update-crontab #{application}"
+#   end
+# end
 
 #############################################################
 # Passenger
