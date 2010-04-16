269f80d6dc2d68a8e6c9c102645a06907e9d9b09
sayale7 <thomas.kohler@gmx.com>
2010-04-16 22:04:16 +0200

    tinymce

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,19 @@
 class PagesController < ApplicationController
   
+  uses_tiny_mce :options => {
+                               :theme => 'advanced',
+                               :plugins => %w{ safari spellchecker pagebreak style layer table save advhr advimage advlink emotions iespell inlinepopups insertdatetime preview media searchreplace print contextmenu paste directionality fullscreen noneditable visualchars nonbreaking xhtmlxtras template },
+                               :theme_advanced_buttons1 => %w{save, bold italic underline strikethrough | justifyleft justifycenter justifyright justifyfull | styleselect formatselect fontselect fontsizeselect},
+                               :theme_advanced_buttons2 => %w{cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,cleanup,help, image,|,code},
+                               :theme_advanced_buttons3 => %w{ablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen,insertdate,inserttime,preview,|,forecolor,backcolor},
+                               :theme_advanced_buttons4 => %w{insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage},
+                               :theme_advanced_toolbar_location => 'top',
+                               :theme_advanced_toolbar_align => "left",
+                               :theme_advanced_statusbar_location => "bottom",
+                               :theme_advanced_resizing => false
+                             }
+  
+  
   access_control do   
     allow :admin
     allow logged_in, :to => [:index, :show]
